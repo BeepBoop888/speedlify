a14e7e8ac48319f8d2ff5c820981b18d348dfed1
BeepBoop888 <61386219+BeepBoop888@users.noreply.github.com>
2023-09-13 16:34:52 +0200

    Create knowers.js

---
+++ _data/sites/knowers.js
@@ -0,0 +1,11 @@
+module.exports = {
+	name: "Knowers", // optional, falls back to object key
+	description: "The non default sites that get tested",
+	options: {
+		runs: 1,
+		frequency: 1, // (in minutes)
+	},
+	urls: [
+		"https://warm-zabaione-85b038.netlify.app/"
+	]
+};
