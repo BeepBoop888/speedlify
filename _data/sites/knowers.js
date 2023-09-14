aeb7c214f67b01498151f39e6868cef447d827d2
BeepBoop888 <61386219+BeepBoop888@users.noreply.github.com>
2023-09-14 12:26:26 +0200

    Update knowers.js
    Set up speedlify-score for each individual site

--- _data/sites/knowers.js
+++ _data/sites/knowers.js
@@ -6,6 +6,7 @@ module.exports = {
 		frequency: 1, // (in minutes)
 	},
 	urls: [
-		"https://warm-zabaione-85b038.netlify.app/"
+		"https://warm-zabaione-85b038.netlify.app/",
+		"https://warm-zabaione-85b038.netlify.app/blog/"
 	]
 };
